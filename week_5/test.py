00765e6d83b707ade9e7cf295e7a1cb1672cd2af
Haowen Zhang <aimsecond12@gmail.com>
2018-05-11 05:10:08 +0000

    create folder week_6

--- week_5/test.py
+++ week_5/test.py
@@ -5,15 +5,16 @@ from memory_profiler import profile
 import time
 
 t=time.time()
-p=subprocess.Popen('ipy ps.py 10000', creationflags =subprocess.CREATE_NEW_CONSOLE)
-# p=subprocess.call('ipy ps.py 10000', creationflags =subprocess.CREATE_NEW_CONSOLE)
-for p in psutil.process_iter(attrs=['pid', 'name']): 
-	while True:
-		if 'ipy' in p.info['name']:
-			pi=p.info['pid']
-			print 'overall cpu time:',psutil.Process(pi).cpu_times()
-			print 'CPU load: ',psutil.cpu_percent(percpu=True)
-			print psutil.Process(pi).memory_info().rss
-		else:
-			break
+p=subprocess.Popen('pypy binary-trees.py 14', shell = True)
+# p=subprocess.call('ipy ps.py 10000', shell = true)
+print p.pid, psutil.Process(p.pid).name()
+# for p in psutil.process_iter(attrs=['pid', 'name']): 
+# 	while True:
+# 		if 'pypy' in p.info['name']:
+# 			pi=p.info['pid']
+# 			print 'overall cpu time:',psutil.Process(pi).cpu_times()
+# 			print 'CPU load: ',psutil.cpu_percent(percpu=True)
+# 			print psutil.Process(pi).memory_info().rss #Find a PeakProcessMemoryUsed
+# 		else:
+# 			break
 print time.time()-t
